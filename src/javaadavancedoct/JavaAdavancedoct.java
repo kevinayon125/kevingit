e0556ceccdff3c25977145cd01f273cc28346c4e
[kevinayon] <keevin_al@hotmail.com>
2017-10-20 15:02:33 -0600

    Initial commit

---
+++ src/javaadavancedoct/JavaAdavancedoct.java
@@ -0,0 +1,21 @@
+public class Artista{
+    private String Nombre, GeneroMusical, Sexo;
+    private int Edad;
+    
+ public String getNombre (){
+     return Nombre;
+ }
+ public String getGeneroMusical (){
+     return GeneroMusical;
+ }
+  public String getSexo (){
+     return Sexo;
+ }
+   public int getEdad (){
+     return Edad;
+ }
+   public boolean setNombre (String Nombre){
+       if(!Nombre.isEmpty())
+          
+   }         
+   }
